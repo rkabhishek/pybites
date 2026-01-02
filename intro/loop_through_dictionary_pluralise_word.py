d79a35970e93a961f281451f6fff60b30c99e0b8
Abhishek Kaushik <akaushik079@gmail.com>
2026-01-02 19:33:37 +0530

    remove comments

--- intro/loop_through_dictionary_pluralise_word.py
+++ intro/loop_through_dictionary_pluralise_word.py
@@ -1,16 +1,4 @@
 def print_game_stats(games_won):
-    """Loop through games_won's dict (key, value) pairs (dict.items)
-       printing (print, not return) how many games each person has won,
-       pluralize 'game' based on number.
-
-       Expected output (ignore the docstring's indentation):
-
-       sara has won 0 games
-       bob has won 1 game
-       tim has won 5 games
-       julian has won 3 games
-       jim has won 1 game
-    """
     for name, num_games in games_won.items():
         suffix = 'game' if num_games == 1 else 'games'
         print(f'{name} has won {num_games} {suffix}')
