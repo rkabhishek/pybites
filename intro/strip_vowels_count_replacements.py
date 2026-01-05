ad648ef82ecfdafa5d5640a4f1f2d914aa5106de
Abhishek Kaushik <akaushik079@gmail.com>
2026-01-05 15:08:51 +0530

    Strip out vowels and count the number of replacements: use enumerate instead of range for clearer iteration

--- intro/strip_vowels_count_replacements.py
+++ intro/strip_vowels_count_replacements.py
@@ -3,8 +3,8 @@ VOWELS = 'aeiou'
 def strip_vowels(text: str) -> tuple[str, int]:
     vowel_count = 0
     text_as_list = list(text)
-    for i in range(len(text_as_list)):
-        if text_as_list[i].lower() in VOWELS:
+    for i, ch in enumerate(text_as_list):
+        if ch.lower() in VOWELS:
             text_as_list[i] = '*'
             vowel_count += 1
 
