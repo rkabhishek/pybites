fa246175b5a90044409c2eff7e5ef50780f3f375
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-03 12:43:03 +0530

    New places to travel to: optimise by using set and symmetric difference

--- regular/new_places_to_travel_to.py
+++ regular/new_places_to_travel_to.py
@@ -1,13 +1,3 @@
 def uncommon_cities(my_cities, other_cities):
-    uncommon = []
+    return len(set(my_cities) ^ set(other_cities))
 
-    for city in my_cities:
-        if city not in other_cities:
-            uncommon.append(city)
-
-
-    for city in other_cities:
-        if city not in my_cities:
-            uncommon.append(city)
-
-    return len(uncommon)
