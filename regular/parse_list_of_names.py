4e14e13dbe7df601cb49903773cafbeed625788a
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-03 15:00:46 +0530

    Parse a list of names: use maxsplit to avoid generating extra strings in case the name has >2 parts

--- regular/parse_list_of_names.py
+++ regular/parse_list_of_names.py
@@ -9,10 +9,10 @@ def dedup_and_title_case_names(names):
     return [string.capwords(name) for name in set(names)]
 
 def surname(name):
-    return name.split()[-1]
+    return name.rsplit(maxsplit=1)[-1]
 
 def len_first(name):
-    return len(name.split()[0])
+    return len(name.split(maxsplit=1)[0])
 
 def sort_by_surname_desc(names):
     deduped_names = dedup_and_title_case_names(names)
@@ -22,4 +22,4 @@ def sort_by_surname_desc(names):
 def shortest_first_name(names):
     deduped_names = dedup_and_title_case_names(names)
     shortest_name = min(deduped_names, key=len_first)
-    return shortest_name.split()[0]
+    return shortest_name.split(maxsplit=1)[0]
