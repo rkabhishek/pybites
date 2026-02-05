9ccd41ad5441749ab5fe47fc976bbe78a0932aa9
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-06 00:05:48 +0530

    Matching multiple strings: optimise membership check by using sets

--- regular/matching_multiple_strings.py
+++ regular/matching_multiple_strings.py
@@ -1,15 +1,17 @@
 VOWELS = 'aeiou'
 PYTHON = 'python'
 
+VOWEL_SET = set(VOWELS)
+PYTHON_SET = set(PYTHON)
 
 def contains_only_vowels(input_str):
     input_str = input_str.lower()
-    return all(ch in VOWELS for ch in input_str)
+    return all(ch in VOWEL_SET for ch in input_str)
 
 
 def contains_any_py_chars(input_str):
     input_str = input_str.lower()
-    return any(ch in PYTHON for ch in input_str)
+    return any(ch in PYTHON_SET for ch in input_str)
 
 
 def contains_digits(input_str):
