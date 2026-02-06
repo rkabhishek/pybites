bacaba8a5f5ecac319d319a165577ffbf075c98b
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-06 16:25:04 +0530

    Rotate string characters: add solution using deque

--- regular/rotate_string_characters.py
+++ regular/rotate_string_characters.py
@@ -1,4 +1,11 @@
+from collections import deque
+
 def rotate(string, n):
     cycle_length = len(string)
     n = n % cycle_length
     return string[n:] + string[:n]
+
+def rotate_deque(string, n):
+    d = deque(string)
+    d.rotate(-n)
+    return ''.join(d)
