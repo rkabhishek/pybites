0d0959534667d0fe60763612cff6ede65c7aaaa6
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-09 14:28:49 +0530

    Pybites date generator: add solution

---
+++ regular/pybites_date_generator.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+
+PYBITES_BORN = datetime(year=2016, month=12, day=19)
+
+def gen_special_pybites_dates():
+    date = PYBITES_BORN
+    while True:
+        date = date + timedelta(days=100)
+        yield date
