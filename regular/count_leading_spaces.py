3b41c7d2b69bf17aac955d757ee1fb4300e85430
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-11 19:35:07 +0530

    Count leading spaces: add solution that counts leading spaces manually

--- regular/count_leading_spaces.py
+++ regular/count_leading_spaces.py
@@ -1,2 +1,12 @@
 def count_indents(text: str) -> int:
+    count = 0
+    for ch in text:
+        if ch == ' ':
+            count += 1
+        else:
+            break
+
+    return count
+
+def count_indents_using_length(text: str) -> int:
     return len(text) - len(text.lstrip(" "))
