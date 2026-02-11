1b9cb52fd250c6a2859cf9f6a990f8e130a5c3d5
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-11 23:02:47 +0530

    Simple length converter: refactor to use dictionary-based unit conversion

--- regular/simple_length_converter.py
+++ regular/simple_length_converter.py
@@ -1,14 +1,13 @@
 from typing import Union
 
+FORMATS = {"cm": 2.54, "in": 0.3937007874}
+
 def convert(value: Union[int, float], fmt: str) -> float:
     if type(value) not in (int, float):
-        raise TypeError(f"{value} must be an int or float")
+        raise TypeError(f"{value} must be an int or float!")
 
     fmt = fmt.lower()
-    if fmt not in ("cm", "in"):
-        raise ValueError(f"fmt must be either 'cm' or 'in'")
+    if fmt not in FORMATS:
+        raise ValueError(f"{fmt} is an unsupported format!")
 
-    if fmt == "cm":
-        return round(value * 2.54, 4)
-    else:
-        return round(value / 2.54, 4)
+    return round(value * FORMATS[fmt], 4)
