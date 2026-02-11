31cb478ee19a15038f96d0b34f244686365d5b5f
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-11 12:34:09 +0530

    Remove punctuation characters from a string: use hash set for punctuation lookup to avoid repeated linear scans

--- regular/remove_punctuation_characters_from_string.py
+++ regular/remove_punctuation_characters_from_string.py
@@ -1,9 +1,9 @@
 def remove_punctuation(input_string):
-    punctuation_string = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+    punctuation_set = set("!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~")
     non_punctuation_char_list = []
 
     for ch in input_string:
-        if ch not in punctuation_string:
+        if ch not in punctuation_set:
             non_punctuation_char_list.append(ch)
     return ''.join(non_punctuation_char_list)
 
