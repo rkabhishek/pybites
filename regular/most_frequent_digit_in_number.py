e15554e045c75c32ad4ecf0957b84928b9db9e3d
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-14 15:48:01 +0530

    Most frequent digit in number: replace sorting with single-pass max-frequency tracking

--- regular/most_frequent_digit_in_number.py
+++ regular/most_frequent_digit_in_number.py
@@ -2,10 +2,15 @@ from collections import defaultdict
 
 def freq_digit(num: int) -> int:
     counts = defaultdict(int)
-    num = str(num)
+    num = str(abs(num))
+
+    result = None
+    max_freq = 0
 
     for c in num:
         counts[c] += 1
+        if counts[c] > max_freq:
+            max_freq = counts[c]
+            result = c
 
-    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-    return int(sorted_counts[0][0])
+    return int(result)
