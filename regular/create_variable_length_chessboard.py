fe46a04e4e5ce866ee786f80811e9851da0a2161
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-15 15:49:59 +0530

    Create a variable length chessboard: derive color from row and column indices and avoid state variables

--- regular/create_variable_length_chessboard.py
+++ regular/create_variable_length_chessboard.py
@@ -2,13 +2,11 @@ WHITE, BLACK = ' ', '#'
 
 
 def create_chessboard(size=8):
-    seq = (WHITE, BLACK)
-    row_index = 0
     for row in range(size):
-        col_index = row_index
         for col in range(size):
-            print(seq[col_index], end='')
-            col_index = 1 - col_index
+            if (row + col) % 2 == 0:
+                print(WHITE, end='')
+            else:
+                print(BLACK, end='')
 
         print()
-        row_index = 1 - row_index
