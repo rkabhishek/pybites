caf69928ce36f7746c8eca7a7b780ba7a2d00ab6
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-15 23:48:58 +0530

    Add type hints to a function: add solution

---
+++ regular/add_type_hints_to_function.py
@@ -0,0 +1,9 @@
+def sum_numbers(numbers: list[int]) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
